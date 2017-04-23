525fdd9fdf286f3903e3790ab2db9f7cdc546234
Aldéric Hoarau <alderikhoarau@yahoo.fr>
2017-04-23 15:42:13 +0200

    test angular translate

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -7,7 +7,7 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(public translate: TranslateService) {
+  constructor(private translate: TranslateService) {
     translate.addLangs(["en", "es", "fr"]);
     translate.setDefaultLang('fr');
 
